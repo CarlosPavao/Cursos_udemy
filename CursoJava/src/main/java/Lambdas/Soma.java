bbfedfec1b3b77455bd2628f06f7f3c424376aff
Carlos Pavão <carlos.henrique93@msn.com>
2021-03-30 10:21:00 -0300

    Inicio lambdas

---
+++ CursoJava/src/main/java/Lambdas/Soma.java
@@ -0,0 +1,14 @@
+package Lambdas;
+
+/**
+ *
+ * @author Carlos Pavão <carlos.henrique93@msn.com>
+ */
+public class Soma implements Calculo {
+
+    @Override
+    public double executar(double a, double b) {
+        return a + b;
+    }
+
+}
