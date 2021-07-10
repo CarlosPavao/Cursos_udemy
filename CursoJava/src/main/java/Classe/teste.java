9eccfbc43f66e355ae72bb05bc8521b578700259
Carlos Pavão <carlos.henrique93@msn.com>
2021-07-10 13:08:27 -0300

    Create teste.java

---
+++ CursoJava/src/main/java/Classe/teste.java
@@ -0,0 +1,17 @@
+package Classe;
+
+import java.util.Random;
+
+/**
+ *
+ * @author Carlos Pavão <carlos.henrique93@msn.com>
+ */
+public class teste {
+    public static void main(String[] args) {
+         
+        
+        
+        System.out.println(teste);
+            
+    }
+}
